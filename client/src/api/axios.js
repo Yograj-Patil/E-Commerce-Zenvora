b0c8c738495efd0e1941f9917ccfa405dddf9934
Yograj-Patil <yograjpatil364@gmail.com>
2026-06-19 01:37:05 +0530

    Update API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://zenvora-backend.onrender.com/api",
 });
 
 export default API;
